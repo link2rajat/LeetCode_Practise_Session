8b5192fafebbb886874798ecbaf0bb43178c0251
Rajat Rathi <linktorajatrathi@gmail.com>
2021-10-25 23:36:46 -0700

    Time: 5 ms (23.44%), Space: 38.9 MB (61.76%) - LeetHub

---
+++ string-compression/string-compression.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int compress(char[] chars) { 
+        
+        int index=0;
+        int start =0;
+        
+        while(start<chars.length)
+        {
+            int end=start;
+            
+            while(end<chars.length && chars[start]==chars[end])
+            {
+              end++;  
+            }
+            
+            chars[index++]=chars[start];
+            if(end-start>1)
+            { 
+                // " " addition to converting count to string since count can be in multiples
+                String count = end -start+"";
+                
+                for(char ch : count.toCharArray())
+                {
+                    chars[index++]= ch;
+                }
+                
+            }
+            
+            start=end;
+            
+        }
+        
+        return index;
+        
+        //T-O(n),S-O(1)
+    }
+}
