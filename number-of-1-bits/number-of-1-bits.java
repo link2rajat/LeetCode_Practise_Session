8515fc857d03b92cd170679f6db871d13ba90538
Rajat Rathi <linktorajatrathi@gmail.com>
2021-11-04 23:35:23 -0700

    Time: 1 ms (47.57%), Space: 35.9 MB (75.58%) - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        
+        int count=0;
+     String str = Integer.toBinaryString(n); 
+        
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)=='1')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}//T-O(n),S-O(n)
