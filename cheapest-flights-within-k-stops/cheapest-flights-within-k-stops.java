20994d089b58b9c7948e248a24b2b7e060910172
Rajat Rathi <linktorajatrathi@gmail.com>
2021-11-17 01:08:20 -0800

    Time: 4 ms (82.18%), Space: 39.7 MB (81.74%) - LeetHub

---
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        
+        int[][] adj = new int[n][n];
+        
+        for(int [] flight: flights)
+        {
+            adj[flight[0]][flight[1]]=flight[2];
+        }
+        //Sort based on min cost
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((n1,n2)->(n1[0]-n2[0]));
+        
+        minHeap.add(new int[]{0,src,0});
+        
+        HashMap<Integer,int[]> seen = new HashMap<>();
+        while(!minHeap.isEmpty())
+        {
+            int[] flightstation = minHeap.poll();
+            
+            int cost = flightstation[0], stopCount=flightstation[2];
+            
+            if(seen.containsKey(flightstation[1]) && seen.get(flightstation[1])[0]<cost &&seen.get(flightstation[1])[1]<stopCount)continue;
+            seen.put(flightstation[1], new int[]{cost,stopCount});
+            if(flightstation[1]==dst)
+            {
+                return cost;
+            }
+            if(stopCount>k)
+            {
+            continue;
+            }
+            
+               for(int nextStation=0;nextStation<n;nextStation++)
+               {
+                   if(adj[flightstation[1]][nextStation]>0)
+                   {
+                       minHeap.add(new int[]{cost+adj[flightstation[1]][nextStation],nextStation,stopCount+1});
+                   }
+               }
+            
+        }
+        
+        return -1;
+    }
+}//T-O(V^2 * log V),S-O(V^2)V is space occupied by adjancy matrix and priority queue
