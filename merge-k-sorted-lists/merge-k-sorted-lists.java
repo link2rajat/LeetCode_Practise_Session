e3b3e4d14b33800943dcfd6105a2265a55c4dbf2
Rajat Rathi <linktorajatrathi@gmail.com>
2021-12-08 15:09:01 -0800

    Time: 3 ms (84.07%), Space: 45 MB (6.40%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.java
+++ merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -3,42 +3,42 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-       
-        if(lists==null || lists.length==0)return null;
-        return mergelist(0,lists.length-1,lists);
-    } 
-    
-    static ListNode mergelist (int start, int end,ListNode[] lists)
-    {
+
+        if(lists.length==0 || lists==null)return null;
+        return mergeList(0,lists.length-1,lists);
         
+    }
+    
+    ListNode mergeList(int start,int end,ListNode[] lists)
+    {
         if(start>=end)
             return lists[start];
         int mid = start+(end-start)/2;
-        ListNode leftList = mergelist(start,mid,lists); 
-        ListNode rightList = mergelist(mid+1,end,lists); 
-              return merge(leftList,rightList);
-     
-    }
-    
-    static ListNode merge(ListNode l,ListNode r)
-    {
-        if(l==null)
-            return r;
-        if(r==null)
-            return l;
-        if(l.val<r.val)
-        {
-         l.next= merge(l.next,r);
-            return l;
-        }
-           r.next= merge(l,r.next);
-            return r;
-    }
+        ListNode l1 = mergeList(start,mid, lists);
+        ListNode l2 = mergeList(mid+1,end,lists);
+      return  merge(l1,l2);
 
+    }
     
-}//T-O(nlogk),S-O(1) where k lis the number of lists and n is the total number of nodes in two lists
+    ListNode merge(ListNode l1, ListNode l2)
+    {
+        if(l1== null)
+            return l2;
+       if(l2== null)
+            return l1;
+        if(l1.val< l2.val)
+        {
+            l1.next=merge(l1.next,l2);
+            return l1;
+        }
+            l2.next=merge(l1,l2.next);
+            return l2;
+    }
+}//T-O(nlogk),S-O(1)
