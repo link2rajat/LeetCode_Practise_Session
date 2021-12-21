57dc8d93e5352ac75ed5b7896df1ff4bf0176cd3
Rajat Rathi <linktorajatrathi@gmail.com>
2021-12-20 21:37:04 -0800

    Time: 4 ms (84.16%), Space: 39.2 MB (99.96%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        
+        
+        Map<Integer,List<Integer>> graph = new HashMap<>();
+        
+       int[] inDegree = new int[numCourses];
+        
+        
+        for(int i=0;i<numCourses;i++)
+        {    
+            
+            graph.putIfAbsent(i,new ArrayList<>());
+            
+        }
+        
+        
+        for(int i=0;i<prerequisites.length;i++)
+        {
+   
+            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            inDegree[prerequisites[i][0]]++;
+        }
+        
+        int[] order = new int[numCourses];
+ 
+        Queue<Integer> queue = new LinkedList<>();
+        
+         for(int i=0;i<numCourses;i++)
+        {    
+             if(inDegree[i]==0)
+             {
+               queue.add(i);  
+             }
+            
+        }
+
+        int count=0;
+
+        while(!queue.isEmpty())
+        {
+            int course = queue.poll();
+            order[count++]=course;
+           
+            if(graph.containsKey(course))
+            {
+                
+                for(Integer neighbour : graph.get(course))
+                {
+                    inDegree[neighbour]--;
+                 if(inDegree[neighbour]==0)
+                 {
+                    queue.add(neighbour);  
+                 }
+                }
+            }
+        }
+            
+        if(count==numCourses)
+        {
+            return order;
+        }
+        
+        return new int[0];
+            
+    }
+}//T-_O(V+E),S-O(V+E)
