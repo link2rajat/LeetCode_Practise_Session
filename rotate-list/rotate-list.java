508d06abf926d85682b853e101a74852aee95ffe
Rajat Rathi <linktorajatrathi@gmail.com>
2021-12-25 17:07:29 -0800

    Time: 0 ms (100.00%), Space: 38.3 MB (77.91%) - LeetHub

--- rotate-list/rotate-list.java
+++ rotate-list/rotate-list.java
@@ -3,33 +3,38 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        
-        if(head==null)return null;
-        if(head.next==null)return head;
-        
-        ListNode old_tail = head;
-        
-        int n;
-        for(n=1;old_tail.next != null;n++)
+    
+        if(head==null) return null;
+
+        if(head.next==null) return head;
+
+        ListNode  oldhead = head;
+       int size;
+        for(size=1;oldhead.next != null;size++)
         {
-            old_tail=old_tail.next;
+          oldhead = oldhead.next;
         }
-        old_tail.next=head;
+        oldhead.next =head;
+
         
-        ListNode new_tail = head;
-        for(int i=0;i<n-k%n-1;i++)
+         ListNode  newtail = head;
+    
+        for(int i= 0;i<size-k%size-1;i++)
         {
-            new_tail = new_tail.next;
+            newtail=newtail.next;
         }
-        ListNode new_head = new_tail.next;
-        new_tail.next=null;
+          ListNode newhead = newtail.next;
         
-        return new_head;
+        newtail.next = null;
         
+        
+        return newhead;
     }
-}//T-O(N),S-O(1)//n is length of LinkedList
+}//T-O(N),S-O(1)
