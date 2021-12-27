70ac3da500b66f04bec6cea9f5b5344246130425
Rajat Rathi <linktorajatrathi@gmail.com>
2021-12-26 16:18:23 -0800

    Time: 87 ms (59.60%), Space: 58.8 MB (66.60%) - LeetHub

---
+++ ipo/ipo.java
@@ -0,0 +1,37 @@
+//Sort capital based on profits,
+//with w take capital which has highest profit
+//deduct it from list and with next progit
+//again take from cap which has highed profit 
+// repeat this step till k projects
+
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        
+         //pq with inc order of cap   
+        PriorityQueue<int[]> pqCap = new  PriorityQueue<>((a1,a2)->(a1[0]-a2[0]));
+        // pq with dec order of pro
+        PriorityQueue<int[]> pqPro = new  PriorityQueue<>((b1,b2)->(b2[1]-b1[1]));
+
+        for(int i=0;i<profits.length;i++)
+        {
+            pqCap.offer(new int[]{capital[i],profits[i]});
+        }
+        
+        
+        for(int j=0;j<k;j++)
+        {
+            while(!pqCap.isEmpty() && pqCap.peek()[0] <=w)
+            {
+                pqPro.offer(pqCap.poll());
+            }
+            
+            if(pqPro.isEmpty()) 
+                break;
+          
+            w+= pqPro.poll()[1];
+        }
+        
+        return w;
+        
+    }
+}//T-O(Nlogk),S-O(N)
