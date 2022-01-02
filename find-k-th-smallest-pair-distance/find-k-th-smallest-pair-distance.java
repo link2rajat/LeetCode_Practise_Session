e98faa613ba5e9799d95073a407bdc47a198b993
Rajat Rathi <linktorajatrathi@gmail.com>
2022-01-01 17:06:30 -0800

    Time: 3 ms (100.00%), Space: 39.1 MB (57.65%) - LeetHub

--- find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,37 +1,36 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
-    
-     Arrays.sort(nums);
-     int low =0;
-     int high = nums[nums.length-1]-nums[0];
-
+        
+        Arrays.sort(nums);
+        
+        int low = 0;
+        int high =nums[nums.length-1]-nums[0];
+        
         while(low<high)
-            {
-       
-            int mid=(low+high)/2;
+        {
+            int mid = (low+high)/2;
             
-            int count=0,winStart=0;
+            int winStart=0,count=0;
+            
             for(int winEnd=0;winEnd<nums.length;winEnd++)
             {
-          
-            while(nums[winEnd]-nums[winStart]>mid)
-            {
-                 winStart++;
-            }
-                 count+=winEnd-winStart;
-            }
-                
-                if(count>=k)
+                while(nums[winEnd]-nums[winStart]>mid)
                 {
-                  high = mid;
+                  winStart++;
                 }
-                else{
-                    low= mid+1;
-                }
+              count+=winEnd-winStart;
+ 
             }
             
-        
-     return low;   
-        
+            if(count>=k)
+            {
+                high=mid;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return low;
     }
-}// T-O(NlogW+NlogN) where W is nums[nums.length-1]-nums[0], S-O(1)
+}//T-O(NLogW+ NLogN),S-O(1)
