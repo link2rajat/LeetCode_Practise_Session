f63bfe75904363fcc1506e798da5bcb164d02343
Rajat Rathi <linktorajatrathi@gmail.com>
2022-10-22 19:52:05 -0700

    Time: 52 ms (6.38%), Space: 60.7 MB (5.11%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+   
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        int dup=-1,miss=0;
+        for(int num: nums)
+        {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        
+        for(int i=1;i<=nums.length;i++)
+        {
+            if(map.containsKey(i))
+            {
+                if(map.get(i)==2)
+                {
+                    dup=i;
+                }
+            }
+            else
+            {
+                miss=i;
+            }
+        }
+        
+        return new int[]{dup,miss};
+        
+    }
+}//T-O(n),S-O(n)
