481f84b363841a0da6460aa60afb7ca1bc441594
Rajat Rathi <linktorajatrathi@gmail.com>
2022-12-18 16:33:00 -0800

    Time: 113 ms (75.84%), Space: 139.9 MB (78.02%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination){
+
+        Map<Integer,List<Integer>> graph = new HashMap<>();
+        
+        constructGraph(n,edges,graph);
+        boolean[] visited = new boolean[n];
+        return findPath(graph,source,destination, visited);
+    }//T-O(n),S-O(n)
+    
+    void constructGraph(int n,int[][] edges,Map<Integer,List<Integer>> graph){
+        for(int index=0;index<n;index++){
+            graph.put(index,new ArrayList<>());
+        }
+        for(int[] edge : edges){
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+    }
+    
+    boolean findPath(Map<Integer,List<Integer>> graph, int source, int destination, boolean[] visited){
+        
+        Queue<Integer> bfsQueue = new LinkedList<>();
+        bfsQueue.add(source);
+        visited[source]=true;
+        
+        while(!bfsQueue.isEmpty()){
+            Integer vertexCurrentVisit = bfsQueue.poll();
+            if(vertexCurrentVisit==destination){
+                    return true;
+                }
+            for(Integer neighbour: graph.get(vertexCurrentVisit)){
+                if(!visited[neighbour])
+                   { 
+                visited[neighbour]=true;
+                bfsQueue.add(neighbour);
+                }
+            }
+        }
+        return false;
+    }
+}
