9ad862312c259fc34a69eb3def83f901e87bb0e4
Rajat Rathi <linktorajatrathi@gmail.com>
2022-12-24 16:43:11 -0800

    Time: 5 ms (97.08%), Space: 43.2 MB (77.45%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -20,7 +20,7 @@ class Solution {
         int left =0;
         int right = nums.length-1;
         
-        while(left<right){
+        while(left<=right){
             int mid = left+(right-left)/2;
             if(nums[mid]<=target){
                 left = mid+1;
@@ -29,7 +29,7 @@ class Solution {
                 right = mid-1;
             }
         }
-        return nums[left]>target ? left :left+1;
+        return left;
     }
     
 }
