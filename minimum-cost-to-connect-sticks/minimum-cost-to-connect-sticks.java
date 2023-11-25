962eef4a4e018bde9c2fee66faf410fcfa4d837a
Rajat Rathi <linktorajatrathi@gmail.com>
2023-11-25 12:58:23 -0800

    Time: 58 ms, Memory: 44.6 MB - LeetHub

--- minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.java
+++ minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.java
@@ -1,24 +1,18 @@
 class Solution {
     public int connectSticks(int[] sticks) {
-        if(sticks.length==0 || sticks.length==1 )
-            return 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        
-        for(int stick: sticks)
-        {
+        for(int stick:sticks){
             pq.offer(stick);
         }
-          int result=0;
-        while(pq.size()>1)
-        {
-            int top = pq.poll();
-            int top_2 = pq.poll();
-            int new_max = top+top_2;
-            result+=new_max;
-            pq.offer(new_max);
+        int ans=0;
+        while(pq.size()>1){
+            int stick1= pq.remove();
+            int stick2=pq.remove();
+            int sum= stick1+stick2;
+            ans+=sum;
+            pq.offer(sum);
         }
-        
-        return result;
+        return ans;
     }
-}//T-O(nlogk), S-O(n)
+}
