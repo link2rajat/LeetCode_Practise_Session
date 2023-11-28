5f8234ee8c8714a384fb626ef895c13127549cfa
Rajat Rathi <linktorajatrathi@gmail.com>
2023-11-27 18:12:25 -0800

    Time: 15 ms (97.35%), Space: 53 MB (65.76%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,15 +1,16 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-      Arrays.sort(people);
-        int i=0,j=people.length-1;
-        int ans=0;
-        
+        Arrays.sort(people);
+        int i=0;
+        int j=people.length-1;
+        int answer=0;
         while(i<=j){
-            ans++;
-            if(people[i]+people[j]<=limit)
+            if(people[i]+people[j]<=limit){
                 i++;
+            }
             j--;
+            answer++;
         }
-        return ans;
+        return answer;
     }
-}//T-O(NLogN),S-O(logN)
+}
