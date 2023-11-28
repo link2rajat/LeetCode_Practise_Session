7fe8d2179d1f233aec7b119e56c5ff544efc34e4
Rajat Rathi <linktorajatrathi@gmail.com>
2023-11-28 15:17:36 -0800

    Time: 36 ms (58.85%), Space: 55.7 MB (74.96%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,41 +1,28 @@
 class Solution {
-    //https://www.youtube.com/watch?v=AFCk84AXvsA&ab_channel=Fraz
     public int minSetSize(int[] arr) {
-        
-        Map<Integer,Integer> countMap = new HashMap<>();
-        int [] list = new int[arr.length+1];
-        
-        for(int ar: arr)
-        {
-          countMap.put(ar,countMap.getOrDefault(ar,0)+1);  
+        Map<Integer,Integer> map = new HashMap<>();
+        int size=arr.length;
+        for(int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        
-        for(int number : countMap.keySet())
-        {
-            int count = countMap.get(number);
-            list[count]+=1;
+         List<Integer> valueList = new ArrayList<>();
+        for(int val:map.values()){
+            valueList.add(val);
         }
+        Collections.sort(valueList);
+          Collections.reverse(valueList);
+       
         
-        int step=0,res=0;
-        
-        for(int i=arr.length;i>0;i--)
-        {
-            int cur=list[i];
-            
-            while(cur-->0)
-            {
-                step+=i;
-                res++;
-                if(step>=arr.length/2)
-                {
-                    return res;
-                }
-                    
+        int numElementRemove=0;
+        int ans =0;
+        for(int val:valueList){
+            numElementRemove+=val;
+            ans++;
+            if(numElementRemove>=size/2){
+                break;
             }
         }
-        
-        return arr.length;
-        
+        return ans;
         
     }
-}//T-O(n),S-O(n)
+}
