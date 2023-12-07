ad8ebf82bd852687e3bbb79b2011ab23f6a0404d
Rajat Rathi <linktorajatrathi@gmail.com>
2023-12-07 13:57:56 -0800

    Time: 0 ms, Memory: 40.7 MB - LeetHub

--- combination-sum-iii/combination-sum-iii.java
+++ combination-sum-iii/combination-sum-iii.java
@@ -2,31 +2,22 @@ class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> candidate = new ArrayList<>();
-        int target = n;
-        for(int i=1;i<=9;i++){
-            candidate.add(i);
-        }
-         List<Integer> subset = new ArrayList<>();
-        backtrack(0,candidate,subset,target,k,ans);
+        backtrack(1,candidate,n,k,ans);
         return ans;
     }
-    void backtrack(int i, List<Integer> candidate, List<Integer> subset
-                   ,int target, int k, List<List<Integer>> ans ){
-        int n=candidate.size();
-        if(target==0 && k==0){
-            ans.add(new ArrayList<>(subset));
+    void backtrack(int start, List<Integer> candidate,int n, int k,List<List<Integer>> ans ){
+        if( k==candidate.size()){
+            if(n==0)
+            ans.add(new ArrayList<>(candidate));
+            return;
         }
         
-        for(int j=i;j<n;j++){
-            if(j>i && candidate.get(j)==candidate.get(j-1)){
-                continue;
-            }
-            if(candidate.get(j)<=target){
-                subset.add(candidate.get(j));
-                  backtrack(j+1,candidate,subset,target-candidate.get(j),k-1,ans);
-                 subset.remove(subset.size()-1);
+        for(int j=start;j<=9;j++){
+                candidate.add(j);
+                backtrack(j+1,candidate,n-j,k,ans);
+                 candidate.remove(candidate.size()-1);
 
-            }
+            
         }
     }
 }
