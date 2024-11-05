bfa10141f5e17d197cfd1f70c4b4f3233372ea07
Rajat Rathi <linktorajatrathi@gmail.com>
2024-11-05 14:00:16 -0500

    Time: 1 ms (95.81%), Space: 41.6 MB (65.18%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
@@ -1,27 +1,27 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        if(word==null || abbr==null)
+          if(word==null || abbr==null)
             return false;
         int i=0,j=0;
-        while(i<word.length() && j<abbr.length()){
-            char c1 = word.charAt(i);
-            char c2 = abbr.charAt(j);
-            
-            if(c1==c2){
+        int wLength= word.length();
+        int aLength= abbr.length();
+        while(i<wLength && j<aLength){
+            char a = word.charAt(i); 
+            char b = abbr.charAt(j);
+            if(a==b){
                 i++;
                 j++;
-            }else if(Character.isDigit(c2) && c2 != '0'){
-                int digitCount = 0;
-                while(j<abbr.length() && Character.isDigit(abbr.charAt(j)) ){
-                    digitCount=digitCount*10 + (abbr.charAt(j)-'0');
-                    j++;
-                }
-                i+=digitCount;
-            }else{
+            }else if(Character.isDigit(abbr.charAt(j)) && abbr.charAt(j) !='0' ){
+                int digitLength=0;
+                    while(j<aLength && Character.isDigit(abbr.charAt(j))){
+                        digitLength=10*digitLength+((abbr.charAt(j))-'0');
+                        j++;
+                    }
+                    i+=digitLength;
+                } else {
                 return false;
-            }
-            
-        }
-        return i==word.length() && j==abbr.length();
+                }
+          }
+           return i==wLength && j==aLength;
     }
 }
