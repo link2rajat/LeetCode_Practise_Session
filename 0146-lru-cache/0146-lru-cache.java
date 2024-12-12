acfcb65cab60ec1f9412e34f1025abf42d904621
Rajat Rathi <linktorajatrathi@gmail.com>
2024-12-12 07:41:54 -0500

    Time: 41 ms (96.29%), Space: 111.2 MB (94.19%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,77 +1,68 @@
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
-import java.util.Hashtable;
-
+import java.util.Hashtable;  
 class LRUCache {
-
+  
     class DoublyLinkedList{
-        int key;
-        int value;
-        DoublyLinkedList prev;
-        DoublyLinkedList next;
+    int key;
+    int value;
+    DoublyLinkedList prev;
+    DoublyLinkedList next;
     }
-   
+    
     DoublyLinkedList head;
     DoublyLinkedList tail;
+    Hashtable<Integer,DoublyLinkedList> cache;
     int size;
     int capacity;
-    Hashtable<Integer,DoublyLinkedList> cache;
     
     void addNode(DoublyLinkedList node){
-        node.prev=head;
-        node.next=head.next;
-        
-        head.next.prev=node;
-        head.next=node;
-        
+    node.prev=head;
+    node.next=head.next;
+    head.next.prev=node;
+    head.next=node;
     }
     
     void removeNode(DoublyLinkedList node){
-        DoublyLinkedList prev = node.prev;
-        DoublyLinkedList next = node.next;
-        
-        prev.next=next;
-        next.prev=prev;
-    }
-    
-    DoublyLinkedList removeTail(){
-        DoublyLinkedList node = tail.prev;
-        removeNode(node);
-        return node;
+    DoublyLinkedList prev = node.prev;
+    DoublyLinkedList next = node.next;
+    prev.next=next;
+    next.prev=prev;
     }
     
     void moveNode(DoublyLinkedList node){
-        removeNode(node);
-        addNode(node);
+    removeNode(node);
+    addNode(node);
     }
     
+    DoublyLinkedList removeTail(){
+    DoublyLinkedList node = tail.prev;
+    removeNode(node);
+    return node;
+    }
+        
+ 
+
     public LRUCache(int capacity) {
-        this.capacity=capacity;
-        size=0;
-        head = new DoublyLinkedList();
-        tail = new DoublyLinkedList();
-        cache = new   Hashtable<Integer,DoublyLinkedList>();
-       
-        head.next=tail;
-        tail.prev=head;
+    cache = new Hashtable<Integer,DoublyLinkedList>();
+    this.capacity=capacity;
+    head = new DoublyLinkedList();
+    tail = new DoublyLinkedList();
+    size=0;
+    head.next=tail;
+    tail.prev=head;    
     }
     
     public int get(int key) {
         DoublyLinkedList node = cache.get(key);
-        
         if(node==null){
-            return -1;
-        }
+        return -1;
+        }        
         moveNode(node);
         return node.value;
+        
     }
     
     public void put(int key, int value) {
-         DoublyLinkedList node= cache.get(key);
+       DoublyLinkedList node= cache.get(key);
         
         if(node==null){
           DoublyLinkedList newNode = new DoublyLinkedList();
@@ -90,6 +81,12 @@ class LRUCache {
            node.value=value;
            moveNode(node);
         }
+    }
 }
-}
-//T-O(1),S-O(n)
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
