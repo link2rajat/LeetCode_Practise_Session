7236eb783690e8606f8c3cc9717398f4a5180e9b
Rajat Rathi <linktorajatrathi@gmail.com>
2024-12-21 19:57:48 -0500

    Time: 2 ms (41.30%), Space: 44.9 MB (57.91%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,29 +1,26 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new LinkedList<int[]>();
-
+        List<int[]> result = new LinkedList<>();
+        
         int i=0;
-        while(i<intervals.length && intervals[i][1]<newInterval[0])
-        {
-                result.add(intervals[i++]);
+        
+        while(i<intervals.length && intervals[i][1]<newInterval[0] ){
+            result.add(intervals[i++]);
         }
         
-        int[] mergeInterval = newInterval;
         
-        while(i<intervals.length && intervals[i][0]<=mergeInterval[1])
-        {
-                mergeInterval[0] = Math.min(intervals[i][0],mergeInterval[0]);
-                mergeInterval[1] = Math.max(intervals[i++][1],mergeInterval[1]);
-        }
-        result.add(mergeInterval);
-        
-         while(i<intervals.length)
-        {
+        while(i<intervals.length && intervals[i][0]<=newInterval[1] ){
+            newInterval[0]= Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1]= Math.max(intervals[i++][1],newInterval[1]);
             
-                result.add(intervals[i++]);
+        }
+        result.add(newInterval);
+        
+        while(i<intervals.length ){
+            result.add(intervals[i++]);
         }
         
-        return result.toArray(new int [result.size()][2]);
-        
+        return result.toArray(new int [result.size()][]);
+
     }
 }//T-O(n),S-O(n)
